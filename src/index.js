12f07d85fd3d42b5b790af17313d691e831c654a
gohai <gottfried.haider@gmail.com>
2023-06-06 18:56:06 +0200

    Add utils/p5PreloadHelper.js
    This is needed by ImageClassifier. Copied without changes from the old repository.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import neuralNetwork from "./NeuralNetwork";
 import * as tf from "@tensorflow/tfjs";
 import * as tfvis from "@tensorflow/tfjs-vis";
 import p5Utils from "./utils/p5Utils";
+import preloadRegister from "./utils/p5PreloadHelper";
+
+const withPreload = {
+};
 
 export default Object.assign(
   { p5Utils },
+  preloadRegister(withPreload),
   {
     tf,
     tfvis,
